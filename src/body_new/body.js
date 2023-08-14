499f0b2c104b442895d58268520ad2b546a062f5
Kingsley Okpo <kngsley2018@gmail.com>
2023-08-13 22:58:27 -0400

    this is the final look of the website , I would be pushing to production now

--- src/body_new/body.js
+++ src/body_new/body.js
@@ -62,7 +62,7 @@ const Luminar = ({value}) => {
          <div onClick={toggleNavbar} style={{display:'flex', alignItems:'center', justifyContent:'center', background:'transparent'}}>
             <Hamburger toggled={isOpen} toggle={setOpen}  />
           </div>
-          <div className="nav-logo" style={{textAlign:'center'}}>Phebean Adeniji</div>
+          <div className="nav-logo" style={{textAlign:'center'}}>Kingsley Okpo</div>
 
           </div>
         <ul className='nav-menu' style={{display : navbarActive?"flex":'none' }}>
@@ -70,17 +70,14 @@ const Luminar = ({value}) => {
           <li><Link to={'/elevator-pitch'} style={{ color:colors[value] }}>Elevator Pitch</Link></li>
           <li><a href="#" style={{ color:colors[value] }}>Portfolio</a></li>
           <li><a href="#" style={{ color:colors[value] }}>About</a></li>
-          <li><a href="#" style={{ color:colors[value] }}>Contact</a></li>
         </ul>
       </div>
       <div className='navbar_alt' style={{ backgroundColor:colors[value], color:text_colors[value] }}>
           <div className="nav_logo" style={{textAlign:'center'}}>Kingsley Okpo</div>
         <ul className='nav-menu_alt' >
           <li><a href="#" style={{ color:text_colors[value] }}>Home</a></li>
-          <li><a href="#" style={{ color:text_colors[value] }}>About</a></li>
-          <li><a href="#" style={{ color:text_colors[value] }}>Services</a></li>
-          <li><a href="#" style={{ color:text_colors[value] }}>Portfolio</a></li>
-          <li><a href="#" style={{ color:text_colors[value] }}>Contact</a></li>
+          <li><Link to={'/elevator-pitch'} style={{ color:text_colors[value] }}>Elevator Pitch</Link></li>
+          
         </ul>
       </div>
     </div>
